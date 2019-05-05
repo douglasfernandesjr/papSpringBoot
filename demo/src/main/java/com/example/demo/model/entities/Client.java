6579ca5311ec0e6c569a360387f6a08cb40ccecc
Douglas Fernandes <douglasfpdasilvajunior@gmail.com>
2019-05-05 15:28:27 -0300

    Adicionar classe

---
+++ demo/src/main/java/com/example/demo/model/entities/Client.java
@@ -0,0 +1,64 @@
+package com.example.demo.model.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import java.util.Objects;
+
+@Entity
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 255)
+    private String name;
+    
+    @Column(nullable = false, length = 30)
+    private String phone;
+    
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	// Métodos equals e hashCode são obrigatórios em classes de Entidade. Pode-se usar as implementações sugeridas pela
+    // IDE de sua escolha.
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Client that = (Client) o;
+        return Objects.equals(id, that.id) &&
+                Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
