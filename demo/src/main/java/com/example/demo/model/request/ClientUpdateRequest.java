5873bb50baf8960992a7011d85a1656584aba20d
Douglas Fernandes Pereira da Silva Jr - DPE <douglas.pereira@iteris.com.br>
2019-05-06 07:57:32 -0300

    atualizar

---
+++ demo/src/main/java/com/example/demo/model/request/ClientUpdateRequest.java
@@ -0,0 +1,17 @@
+package com.example.demo.model.request;
+
+import javax.validation.constraints.NotBlank;
+
+public class ClientUpdateRequest extends ClientCreateRequest {
+
+	@NotBlank(message = "Nome é uma informação obrigatória.")
+	private long id;
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+}
