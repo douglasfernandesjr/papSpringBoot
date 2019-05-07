62566be7516d673caa5fe100e8d9a9fa173bafac
Douglas Fernandes Pereira da Silva Jr - DPE <douglas.pereira@iteris.com.br>
2019-05-07 18:23:21 -0300

    Criação de estrutura de usuário padrão 2

--- demo/src/main/java/com/example/demo/model/entities/User.java
+++ demo/src/main/java/com/example/demo/model/entities/User.java
@@ -43,8 +43,8 @@ public class User {
 		return password;
 	}
 
-	public void setPassword(String phone) {
-		this.password = phone;
+	public void setPassword(String password) {
+		this.password = password;
 	}
 
 
