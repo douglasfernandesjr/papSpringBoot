0c403b4de3bc11b7277cb1f71315d29df60202e6
Douglas Fernandes <douglasfpdasilvajunior@gmail.com>
2019-05-07 23:27:07 -0300

    atualizacao II

--- demo/src/main/java/com/example/demo/model/security/CustomLoginUser.java
+++ demo/src/main/java/com/example/demo/model/security/CustomLoginUser.java
@@ -1,21 +1,16 @@
 package com.example.demo.model.security;
 
-import java.util.ArrayList;
-import java.util.Collection;
-
-import org.springframework.security.core.GrantedAuthority;
-
 import com.example.demo.model.entities.User;
 
 public class CustomLoginUser {
 
 	private static final long serialVersionUID = 1L;
 
-	public CustomLoginUser( User user) {
+	public CustomLoginUser(User user) {
 
 		id = user.getId();
 		username = user.getUsername();
-		password = user.getPassword();
+
 	}
 
 	private Long id;
@@ -23,9 +18,6 @@ public class CustomLoginUser {
 	private String username;
 
 	private String password;
-
-	private Collection<GrantedAuthority> grantedAuthoritiesList = new ArrayList<>();
-
 
 	public Long getId() {
 		return id;
@@ -41,21 +33,6 @@ public class CustomLoginUser {
 
 	public void setUsername(String name) {
 		this.username = name;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public Collection<GrantedAuthority> getGrantedAuthoritiesList() {
-		return grantedAuthoritiesList;
-	}
-	public void setGrantedAuthoritiesList(Collection<GrantedAuthority> grantedAuthoritiesList) {
-		this.grantedAuthoritiesList = grantedAuthoritiesList;
 	}
 
 }
