14ddc737d1cb653e1630567365e9602a9f0da677
Douglas Fernandes <douglasfpdasilvajunior@gmail.com>
2020-01-13 21:51:19 -0300

    atualização com o requestBody

--- project/src/main/java/com/example/project/controller/ClientController.java
+++ project/src/main/java/com/example/project/controller/ClientController.java
@@ -9,6 +9,7 @@ import com.example.project.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,7 +30,7 @@ public class ClientController {
     }
     
     @PostMapping
-	public ClientResponse post(ClientCreateRequest model) {
+	public ClientResponse post(@RequestBody ClientCreateRequest model) {
 		return clientService.createClient(model);
 	}
 
