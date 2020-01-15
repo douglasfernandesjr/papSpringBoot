f6cea2ed494f5b90fb25db4e63b0ecb6dfc22f0a
Douglas Fernandes Pereira da Silva Jr <douglas.pereira@iteris.com.br>
2020-01-15 10:57:47 -0300

    add order

---
+++ project/src/main/java/com/example/project/domain/entities/Order.java
@@ -0,0 +1,27 @@
+package com.example.project.domain.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 255)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "clientId", nullable = false)
+    private Client client;
+}
