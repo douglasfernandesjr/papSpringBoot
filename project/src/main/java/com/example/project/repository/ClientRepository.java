2876fe2f492a68de3f684d3a058890d6b4691881
Douglas Fernandes <douglasfpdasilvajunior@gmail.com>
2020-01-14 22:46:40 -0300

    atualização

--- project/src/main/java/com/example/project/repository/ClientRepository.java
+++ project/src/main/java/com/example/project/repository/ClientRepository.java
@@ -2,10 +2,10 @@ package com.example.project.repository;
 
 import com.example.project.domain.entities.Client;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ClientRepository extends CrudRepository<Client, Integer> {
+public interface ClientRepository extends JpaRepository<Client, Integer> {
     
 }
