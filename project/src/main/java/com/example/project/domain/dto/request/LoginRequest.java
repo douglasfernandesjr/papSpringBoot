c2381f0f8c70da3dfe6101d13b8f69bd42094f1d
Douglas Fernandes <douglasfpdasilvajunior@gmail.com>
2020-01-27 20:32:25 -0300

    criação a atualização para versão final

---
+++ project/src/main/java/com/example/project/domain/dto/request/LoginRequest.java
@@ -0,0 +1,18 @@
+package com.example.project.domain.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequest {
+
+    private String username;
+    
+    private String password;
+
+}
